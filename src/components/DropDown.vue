64a03d5c5e659e31ebe3396841901bade91a357e
tya6i <developertyagi1203@gmail.com>
2024-01-29 13:04:59 +0530

    first commit

---
+++ src/components/DropDown.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ greeting }}</h1>
+    </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+    setup() {
+
+        const greeting = computed(() => {
+            return `Hello from VUE`;
+        });
+
+        return {
+            greeting,
+        };
+    },
+};
+</script>
