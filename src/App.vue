e02d9f5b9686a623ca321a177f0d14d606afa830
Developer-Tyagi <developertyagi1203@gmail.com>
2024-01-29 21:14:13 +0530

    Create dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <DropDown />
+  <DropDown :dropdownOptions="dropdownOptions" />
 </template>
 
 <script>
+import { defineComponent, ref } from 'vue'
 import DropDown from './components/DropDown.vue'
 
-export default {
-  name: 'App',
+export default defineComponent({
   components: {
     DropDown
+  },
+  setup() {
+    const dropdownOptions = ref([
+      { key: 1, value: 'One' },
+      { key: 2, value: 'Two' },
+      { key: 3, value: 'Three' },
+      { key: 4, value: 'Four' },
+      { key: 5, value: 'Five' },
+    ]);
+
+    return {
+      dropdownOptions,
+    };
   }
-}
+})
 </script>
--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,21 +1,60 @@
 <template>
     <div>
-        <h1>{{ greeting }}</h1>
+        <div id="app" class="flex items-center justify-center bg-gray-200 min-h-screen">
+            <div class="relative text-lg w-48">
+                <button
+                    class="flex items-center justify-between px-3 py-2 bg-white w-full border border-gray-500 rounded-lg"
+                    @click="isOptionsOpen = !isOptionsOpen" @blur="isOptionsOpen = false">
+                    <span>{{ selectedOption }}</span>
+                    <svg fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                        class="h-4 w-4 transform transition-transform duration-200 ease-in-out"
+                        :class="isOptionsOpen ? 'rotate-180' : 'rotate-0'">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </button>
+                <transition enter-active-class="transform transition duration-900 ease-custom"
+                    enter-class="-translate-y-1/2 scale-y-0 opacity-0"
+                    enter-to-class="translate-y-0 scale-y-100 opacity-100"
+                    leave-active-class="transform transition duration-300 ease-custom"
+                    leave-class="translate-y-0 scale-y-100 opacity-100"
+                    leave-to-class="-translate-y-1/2 scale-y-0 opacity-0">
+                    <ul v-show="isOptionsOpen"
+                        class="absolute left-0 right-0 mb-4 bg-white divide-y rounded-lg shadow-lg overflow-hidden">
+                        <li v-for="(option, index) in dropdownOptions" :key="index"
+                            class="px-3 py-2 transition-colors duration-300 hover:bg-gray-100"
+                            @mousedown.prevent="setOption(option)">
+                            {{ option.value }}
+                        </li>
+                    </ul>
+                </transition>
+            </div>
+        </div>
     </div>
 </template>
-
+  
 <script>
-import { computed } from 'vue';
+import { ref } from 'vue';
 
 export default {
-    setup() {
-
-        const greeting = computed(() => {
-            return `Hello from VUE`;
-        });
+    props: {
+    dropdownOptions: {
+      type: Array,
+      required: true
+    }
+  },
+    setup(props) {
+        console.log(props.dropdownOptions, 41);
+        var isOptionsOpen = ref(false);
+        var selectedOption = ref("Please select");
+        const setOption = (option) => {
+            selectedOption.value = option.value;
+            isOptionsOpen.value = false;
+        }
 
         return {
-            greeting,
+            isOptionsOpen,
+            selectedOption,
+            setOption
         };
     },
 };
